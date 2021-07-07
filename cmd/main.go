0280d33d3ccf5a12dc7ee1b3b8967fbdc8e109fb
cenkayla <falconsaho105@hotmail.com>
2021-07-08 01:03:53 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect database.")
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+}
